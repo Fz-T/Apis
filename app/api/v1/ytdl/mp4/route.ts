ff093c454640314344429d65782c38cab19516dc
Fz-T <sotoian093@gmail.com>
2025-03-30 21:59:05 -0600

    Update route.ts

--- app/api/v1/ytdl/mp4/route.ts
+++ app/api/v1/ytdl/mp4/route.ts
@@ -1,7 +1,7 @@
  import { NextResponse } from "next/server"
 import axios from "axios"
 
-async function ytdl(url: string) {
+async function ytdl(url) {
   const headers = {
     "accept": "*/*",
     "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -14,34 +14,26 @@ async function ytdl(url: string) {
     "Referer": "https://id.ytmp3.mobi/",
     "Referrer-Policy": "strict-origin-when-cross-origin"
   };
+  const initial = await fetch(`https://d.ymcdn.org/api/v1/init?p=y&23=1llum1n471&_=${Math.random()}`, { headers });
+  const init = await initial.json();
+  const id = url.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/|.*embed\/))([^&?/]+)/)?.[1];
+  const convertURL = init.convertURL + `&v=${id}&f=mp4&_=${Math.random()}`;
 
-  try {
-    const initial = await axios.get(`https://d.ymcdn.org/api/v1/init?p=y&23=1llum1n471&_=${Math.random()}`, { headers });
-    const init = initial.data;
+  const converts = await fetch(convertURL, { headers });
+  const convert = await converts.json();
 
-    const idMatch = url.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/|.*embed\/))([^&?/]+)/);
-    if (!idMatch) throw new Error("No se pudo extraer el ID del video.");
-    const id = idMatch[1];
-
-    const convertURL = `${init.convertURL}&v=${id}&f=mp4&_=${Math.random()}`;
-    const converts = await axios.get(convertURL, { headers });
-    const convert = converts.data;
-
-    let info = {};
-    for (let i = 0; i < 3; i++) {
-      const progressResponse = await axios.get(convert.progressURL, { headers });
-      info = progressResponse.data;
-      if (info.progress === 3) break;
-      await new Promise(resolve => setTimeout(resolve, 2000));
-    }
-
-    return {
-      url: convert.downloadURL,
-      title: info.title
-    };
-  } catch (error: any) {
-    return { status: false, error: error.message };
+  let info = {};
+  for (let i = 0; i < 3; i++) {
+    const progressResponse = await fetch(convert.progressURL, { headers });
+    info = await progressResponse.json();
+    if (info.progress === 3) break;
   }
+
+  const result = {
+    url: convert.downloadURL,
+    title: info.title
+  };
+  return result;
 }
 
 export async function GET(request: Request) {
